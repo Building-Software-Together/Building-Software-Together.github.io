f76db50dcbb44e612660b9da89860a3a5ef1d7cc
iamother <xandrewssx970@outlook.es>
2023-01-24 18:40:08 -0500

    Updated github.io

---
+++ source/js/main.js
@@ -0,0 +1,24 @@
+// Create a array with all teams from Premier League
+const teams = [
+  "Manchester City",
+  "Manchester United",
+  "Liverpool",
+  "Chelsea",
+  "Arsenal",
+  "Tottenham Hotspur",
+  "Everton",
+  "Leicester City",
+  "West Ham United",
+  "Aston Villa",
+  "Wolverhampton Wanderers",
+  "Crystal Palace",
+  "Sheffield United",
+  "Burnley",
+  "Southampton",
+  "Newcastle United",
+  "Brighton & Hove Albion",
+  "Fulham",
+  "West Bromwich Albion",
+  "Leeds United",
+];
+
